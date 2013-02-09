311660ffbd9a0cd6b487c307b7b854257d9222ed
= <matthewhinkle@live.com>
2013-02-08 19:28:46 -0500

    Updating the Xcode project file

---
+++ BamfTests/BamfTests.h
@@ -0,0 +1,13 @@
+//
+//  BamfTests.h
+//  BamfTests
+//
+//  Created by Matthew Hinkle on 1/31/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BamfTests : SenTestCase
+
+@end
---
+++ BamfTests/BamfTests.m
@@ -0,0 +1,36 @@
+//
+//  BamfTests.m
+//  BamfTests
+//
+//  Created by Matthew Hinkle on 1/31/13.
+//
+//
+
+#import "BamfTests.h"
+
+#import "SDL/SDL.h"
+
+@implementation BamfTests
+
+- (void)setUp
+{
+    [super setUp];
+	
+	
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BamfTests");
+}
+
+@end
