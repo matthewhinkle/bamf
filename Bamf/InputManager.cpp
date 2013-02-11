df2766bc44638304218d917e90d555e235f0104c
mikehershey <mikehershey32@gmail.com>
2013-02-11 16:38:59 -0500

    adding merger.sh

---
+++ Bamf/InputManager.cpp
@@ -0,0 +1,9 @@
+//
+//  InputManager.cpp
+//  Bamf
+//
+//  Created by Mike Hershey on 2/11/13.
+//
+//
+
+#include "InputManager.h"
---
+++ Bamf/KeyPressType.h
@@ -0,0 +1,14 @@
+//
+//  KeyPressType.h
+//  Bamf
+//
+//  Created by Mike Hershey on 2/11/13.
+//
+//
+
+#ifndef Bamf_KeyPressType_h
+#define Bamf_KeyPressType_h
+
+
+
+#endif
